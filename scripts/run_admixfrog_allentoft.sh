016455ce06244e6ef8e61bdd2ea3b0864e1c3f6b
Jack Demaray <jackdemaray@n0028.savio2>
2024-10-02 14:30:03 -0700

    update to run on all chromosomes at once

--- scripts/run_admixfrog_allentoft.sh
+++ scripts/run_admixfrog_allentoft.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
+#SBATCH --job-name=admixfrog
+#SBATCH --account=co_moorjani
+#SBATCH --partition=savio3_htc
+#SBATCH --ntasks-per-core=1
+#SBATCH --cores=1
+#SBATCH --output=../scratch/O-%x.%j.out
+#SBATCH --time=12:00:00
+#SBATCH --mem 120gb
+
 # Script to run admixfrog on ancient samples from allentoft et al.
+# this relies on a concatenated vcf file for all autosomes and 
+# calculates introgression for all chromosomes at once
+
 data_dir=../processed-data
 outpath=$data_dir/admixfrog-allentoft/$1
 ref_file=$data_dir/Admixfrog_files/Admixfrog_Reference/ref_archaicadmixtureAPX_hs37mMask35to99.csv
 in_file="$outpath/$1.in.xz"
 mkdir -p $outpath
 
-echo "Running admixfrog on chromosome $2 of sample $1"
+echo "Running admixfrog on sample $1"
 
-admixfrog-bam \
-    --vcfgt /global/scratch/p2p3/pl1_moorjani/SHARED_LAB/DATASETS/hg19/ANCIENT/allentoft_2024/$2.neo_impute_ph.vcf.gz \
-    --chroms $2 \
-    --ref $ref_file \
-    --out "$in_file" \
-    --target $1 --force-target-file
+# admixfrog-bam \
+#     --vcfgt $data_dir/all.neo_impute_ph.vcf.gz \
+#     --ref $ref_file \
+#     --out "$in_file" \
+#     --target $1 --force-target-file
 
 admixfrog --states AFR NEA DEN --cont-id AFR \
     --ll-tol 0.01 --bin-size 5000 --est-F --est-tau --freq-F 3 --freq-contamination 3 \
     --e0 0.01 --est-error --ancestral PAN --run-penalty 0.1 --max-iter 250 \
     --n-post-replicates 200 --filter-pos 50 --filter-map 0.000 \
-    --init-guess AFR \ # Probably safe to keep this in
-    --chroms $2 \
+    --init-guess AFR \
     --infile $in_file \
     --ref $ref_file \
-    --out "$outpath/$2" \
-    --force-target-file 
+    --out "$outpath/$1" \
+    --map-id Shared_Map \
+    --force-target-file --dont-est-contamination
