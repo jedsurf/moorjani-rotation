9fb3a8bc4d622a813e6ae8b32e794166766452bb
Jack Demaray <jackdemaray@n0028.savio2>
2024-10-02 17:15:23 -0700

    first commit

---
+++ src/src.py
@@ -0,0 +1,7 @@
+def legend_kwargs():
+    # Commonly used kwargs for figure legends
+    kwargs = {
+        "frameon" : False, 
+        "bbox_to_anchor" : (1, 0.5), 
+        "loc" : "center left"}
+    return kwargs
